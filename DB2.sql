9da889d2f8c2faef5aa6a3df9fdd3d4026bd0270
Astrid Alins Moragón <astridalinsm@gmail.com>
2026-05-17 10:55:33 +0200

    prelimniar er model

--- DB2.sql
+++ DB2.sql
@@ -14,12 +14,16 @@ CREATE TABLE colla (
 -- =========================
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+
     name VARCHAR(20) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
+
     location VARCHAR(100),
-    userType VARCHAR(20),
+
+    userType ENUM('ADMIN', 'COLLA', 'USER') NOT NULL,
+
     positions VARCHAR(100),
     picture VARCHAR(255),
 
